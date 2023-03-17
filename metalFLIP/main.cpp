d2b692fdcf3d880048114014cbfd081e8eb75d26
arukadya <a19.sj7x@g.chuo-u.ac.jp>
2023-03-17 14:32:43 +0900

    Initial Commit

---
+++ metalFLIP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  metalFLIP
+//
+//  Created by 須之内俊樹 on 2023/03/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
