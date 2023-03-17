3a1e60eb1eb48e5e64eaadb0e29dba6d27fceadb
arukadya <a19.sj7x@g.chuo-u.ac.jp>
2023-03-17 20:07:46 +0900

    prepare

---
+++ metalFLIP/metal-cpp.hpp
@@ -0,0 +1,13 @@
+//
+//  metal-cpp.hpp
+//  metalFLIP
+//
+//  Created by 須之内俊樹 on 2023/03/17.
+//
+
+#ifndef metal_cpp_hpp
+#define metal_cpp_hpp
+
+#include <stdio.h>
+
+#endif /* metal_cpp_hpp */
